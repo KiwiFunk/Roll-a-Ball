342055d5874c4bf3f50f25220d4c7cd19ddefe44
Alex <alex_p97@live.com>
2025-04-09 23:14:23 +0100

    Created PlayerController Script
    Created Script to handle input and apply force to player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine; 
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private float movementX;
+    private float movementY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Assign the Rigidbody component to the rigidBody variable
+        rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(movementX, 0.0f, movementY);
+        // Move the player using the rigidbody component
+        rigidBody.AddForce(movementVector);
+        
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        // Get vector from movementValue and store it in variable
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        // Assign the x and y components of the movement vector to the movementX and movementY variables
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+    
+}
+
